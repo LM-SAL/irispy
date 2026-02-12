9e32109d1f95df8454f29dd54d5cd4d015f293eb
Nabil Freij <nabil.freij@gmail.com>
2026-02-12 15:37:59 -0800

    wobble uses manager to open files

--- irispy/utils/wobble.py
+++ irispy/utils/wobble.py
@@ -64,12 +64,12 @@ def generate_wobble_movie(
 
     Notes
     -----
-    This is designed to be used on IRIS Level 2 SJI data.
+    - This is designed to be used on IRIS Level 2 SJI data.
 
-    2832 is considered the best wavelength to use for wobble movies.
+    - 2832 is considered the best wavelength to use for wobble movies.
 
-    Timestamps take the main header cadence and add that to the "DATE_OBS".
-    They do not use the information in the AUX array.
+    - Timestamps take the main header cadence and add that to the "STARTOBS".
+      They do not use the information in the AUX array.
     """
     # Avoid circular imports
     from irispy.utils import image_clipping  # NOQA: PLC0415
@@ -81,72 +81,71 @@ def generate_wobble_movie(
         files = [files]
     filenames = []
     for a_file in files:
-        data, header = fits.getdata(a_file, header=True)
-        wcs = WCS(header)
-        numframes = header["NAXIS3"]
-        date = header["DATE_OBS"].split(".")[0]
-        # Calculate index to downsample in time to accentuate the wobble
-        cadence = header["CDELT3"]
-        cadence_sample = max(1, np.floor(wobble_cadence / cadence))
-        if timestamp:
+        with fits.open(a_file) as hdulist:
+            data = hdulist[0].data
+            header = hdulist[0].header
+            wcs = WCS(header)
+            numframes = header["NAXIS3"]
+            date = header["STARTOBS"].split(".")[0]
+            # Calculate index to downsample in time to accentuate the wobble
+            cadence = header["CDELT3"]
+            cadence_sample = max(1, np.floor(wobble_cadence / cadence))
             timestamps = [
-                parse_time(header["DATE_OBS"]) + TimeDelta(cadence, format="sec") * i for i in range(numframes)
+                parse_time(header["STARTOBS"]) + TimeDelta(cadence, format="sec") * i for i in range(numframes)
             ]
-        else:
-            timestamps = [parse_time(header["DATE_OBS"])]
-        # Trim down to only that part of the movie that contains data in all frames
-        if trim:
-            # TODO: improve this, it trims a bit but not fully
-            dmin = np.min(data, axis=0)
-            dmask = dmin > -200
-            dmx = np.sum(dmask, axis=1)
-            dmy = np.sum(dmask, axis=0)
-            (subx,) = np.where(dmx > (np.max(dmx) * 0.8))
-            (suby,) = np.where(dmy > (np.max(dmy) * 0.8))
-            data = data[:, suby[0] : suby[-1], subx[0] : subx[-1]]
+            # Trim down to only that part of the movie that contains data in all frames
+            if trim:
+                # TODO: improve this, it trims a bit but not fully
+                dmin = np.min(data, axis=0)
+                dmask = dmin > -200
+                dmx = np.sum(dmask, axis=1)
+                dmy = np.sum(dmask, axis=0)
+                (subx,) = np.where(dmx > (np.max(dmx) * 0.8))
+                (suby,) = np.where(dmy > (np.max(dmy) * 0.8))
+                data = data[:, suby[0] : suby[-1], subx[0] : subx[-1]]
 
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1, projection=wcs.dropaxis(-1))
-        colormap = iris_sji_color_table(str(int(header["TWAVE1"])))
-        vmin, vmax = image_clipping(data)
-        image = ax.imshow(
-            data[0],
-            origin="lower",
-            cmap=colormap,
-            norm=ImageNormalize(vmin=vmin, vmax=vmax, stretch=AsinhStretch()),
-        )
-        ax.set_xlabel("Solar X")
-        ax.set_ylabel("Solar Y")
-        if timestamp:
-            title = ax.text(
-                0.5,
-                0.95,
-                str(timestamps[0]),
-                color="w",
-                transform=ax.transAxes,
-                ha="center",
-                path_effects=[patheffects.withStroke(linewidth=3, foreground="black")],
+            fig = plt.figure()
+            ax = fig.add_subplot(1, 1, 1, projection=wcs.dropaxis(-1))
+            colormap = iris_sji_color_table(str(int(header["TWAVE1"])))
+            vmin, vmax = image_clipping(data)
+            image = ax.imshow(
+                data[0],
+                origin="lower",
+                cmap=colormap,
+                norm=ImageNormalize(vmin=vmin, vmax=vmax, stretch=AsinhStretch()),
             )
-        else:
-            title = ax.text(0.5, 0.95, "")
-
-        def update(i):
-            image.set_array(data[i])  # NOQA: B023
+            ax.set_xlabel("Solar X")
+            ax.set_ylabel("Solar Y")
             if timestamp:
-                title.set_text(str(timestamps[i]))  # NOQA: B023
-            return image, title  # NOQA: B023
+                title = ax.text(
+                    0.5,
+                    0.95,
+                    str(timestamps[0]),
+                    color="w",
+                    transform=ax.transAxes,
+                    ha="center",
+                    path_effects=[patheffects.withStroke(linewidth=3, foreground="black")],
+                )
+            else:
+                title = ax.text(0.5, 0.95, "")
 
-        anim = animation.FuncAnimation(
-            fig,
-            func=update,
-            frames=range(0, numframes, int(cadence_sample)),
-            blit=True,
-            repeat=False,
-            **kwargs,
-        )
-        clean_filename = re.sub(r"[^\w\-_\. ]", "_", f"{header['TDESC1']}_{date}_wobble.mp4")
-        filename = Path(outdir) / Path(clean_filename)
-        writervideo = animation.FFMpegWriter(fps=12)
-        anim.save(filename, writer=writervideo)
-        filenames.append(filename)
+            def update(i):
+                image.set_array(data[i])  # NOQA: B023
+                if timestamp:
+                    title.set_text(str(timestamps[i]))  # NOQA: B023
+                return image, title  # NOQA: B023
+
+            anim = animation.FuncAnimation(
+                fig,
+                func=update,
+                frames=range(0, numframes, int(cadence_sample)),
+                blit=True,
+                repeat=False,
+                **kwargs,
+            )
+            clean_filename = re.sub(r"[^\w\-_\. ]", "_", f"{header['TDESC1']}_{date}_wobble.mp4")
+            filename = Path(outdir) / Path(clean_filename)
+            writervideo = animation.FFMpegWriter(fps=12)
+            anim.save(filename, writer=writervideo)
+            filenames.append(filename)
     return filenames
