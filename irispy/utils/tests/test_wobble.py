b25d44cdc34716f9e4d90603e7cda0c8e3bab264
Nabil Freij <nabil.freij@gmail.com>
2026-03-25 14:43:50 -0700

    Filter warning

--- irispy/utils/tests/test_wobble.py
+++ irispy/utils/tests/test_wobble.py
@@ -1,6 +1,9 @@
+import pytest
+
 from irispy.utils.wobble import generate_wobble_movie
 
 
+@pytest.mark.filterwarnings("ignore:invalid value encountered in do_format")
 def test_generate_wobble_movie(fake_long_sns_obs, tmp_path):
     movies = generate_wobble_movie(fake_long_sns_obs, outdir=tmp_path)
     assert movies != []
