2f02060bb1ccc964c31ebfa80b6da707d5748dc7
Nabil Freij <nabil.freij@gmail.com>
2026-05-08 17:35:39 -0700

    Fix some issues with the code in the cosmic_ray backend and update the main example to be more useful

--- examples/calibration/01_remove_spikes.py
+++ examples/calibration/01_remove_spikes.py
@@ -3,11 +3,17 @@
 Remove Cosmic Ray Hits from IRIS data
 =====================================
 
-This example illustrates how to remove cosmic ray hits from IRIS SJI data.
+This example illustrates how to remove cosmic ray hits from IRIS spectrograph data.
+This can also be done for slit-jaw images, but we will focus on spectra as that is a more
+common use case.
 
-We will use the ``astroscrappy`` backend and has to be installed separately using ``pip`` or ``conda``.
+We will use both ``rsliding`` and ``astroscrappy`` backends, which have to be installed separately using ``pip`` or ``conda``.
 
-``irispy`` supports two backends for cosmic ray removal, they are compared on spectra here: :ref:`sphx_glr_generated_gallery_calibration_04_compare_cosmic_ray_backends.py`.
+To understand both backends, their parameters and how they work, we suggest you read the original
+documentation for each package.
+
+* `rsliding documentation <https://git.ias.u-psud.fr/avoyeux/rsliding>`__
+* `astroscrappy documentation <https://astroscrappy.readthedocs.io/en/latest/>`__
 """
 
 import matplotlib.pyplot as plt
@@ -22,65 +28,80 @@ quantity_support()
 ###############################################################################
 # `We start with getting data from the IRIS data archive <https://www.lmsal.com/hek/hcr?cmd=view-event&event-id=ivo%3A%2F%2Fsot.lmsal.com%2FVOEvent%23VOEvent_IRIS_20260209_215233_3602506433_2026-02-09T21%3A52%3A332026-02-09T21%3A52%3A33.xml>`__.
 #
+# This dataset is during the South Atlantic Anomaly (SAA) passage, which is known to cause a large
+# number of cosmic ray hits in the data.
+#
+# This is what we call, a worse-case scenario for cosmic ray removal, which is good for testing the algorithms but not ideal for science.
+#
 # In this case, we will use ``pooch`` to keep this example self-contained
 # but you can download the data manually using your browser as well.
 #
 # You will need to update the path to the data in the next section if you do that.
 
-sji_filename = pooch.retrieve(
-    "https://www.lmsal.com/solarsoft/irisa/data/level2_compressed/2026/02/06/20260206_210853_3460104433/iris_l2_20260206_210853_3460104433_SJI_2832_t000.fits.gz",
-    known_hash="d5088c6a0753ea9ce7b525865ba2edf13a637097ee995985b511833897c88ca6",
-)
-###############################################################################
-# We will now open the file we just downloaded and select one frame.
-#
-# ``astroscrappy`` works on one 2D frame at a time. Selecting a single frame
-# keeps this example lightweight enough for documentation builds while still
-# demonstrating the same API.
-
-sji_2832 = read_files(sji_filename)
-sji_frame = sji_2832[5]
-del sji_2832
-
-###############################################################################
-# Now we use ``remove_cosmic_rays`` with the ``astroscrappy`` backend.
-#
-# This algorithm can perform well with both high and low noise levels in the original data.
-# This particular image has lots of high intensity cosmic ray hits which
-# cannot be effectively removed by using the default set of parameters.
-# So we reduce ``sigma`` (mapped to astroscrappy's ``sigclip``) from 4.5 to 2 to mark more hits.
-# We also reduce ``objlim``, the contrast between the Laplacian image and the fine structured image
-# to clean the high intensity bright cosmic ray hits.
-# We also modify the ``readnoise`` parameter to obtain better results.
-#
-# For more details on the parameters, see the `astroscrappy documentation <https://astroscrappy.readthedocs.io/en/latest/api/astroscrappy.detect_cosmics.html#astroscrappy.detect_cosmics>`__.
-
-sji_cleaned = sji_frame.remove_cosmic_rays(
-    method="astroscrappy",
-    sigma=2,
-    method_kwargs={"objlim": 2, "readnoise": 4},
+raster_filename = pooch.retrieve(
+    "https://www.lmsal.com/solarsoft/irisa/data/level2_compressed/2026/02/09/20260209_215233_3602506433/iris_l2_20260209_215233_3602506433_raster.tar.gz",
+    known_hash="bad4a3617d0fd04679203951d7db595df196f79b41a3f6f1f71ce0e301486434",
 )
 
 ###############################################################################
-# ``remove_cosmic_rays`` returns a cleaned cube with the same metadata and coordinates.
-# We now plot the noisy frame and the cleaned frame side-by-side.
+# We will now open the data using a helper function which is designed to read
+# all files from a single observation.
 
-fig = plt.figure(figsize=(12, 5), constrained_layout=True)
-
-ax = fig.add_subplot(121, projection=sji_frame.wcs)
-sji_frame.plot(axes=ax, vmin=0, vmax=500)
-ax.set_title("Original")
-
-ax1 = fig.add_subplot(122, projection=sji_cleaned.wcs)
-sji_cleaned.plot(axes=ax1, vmin=0, vmax=500)
-ax1.set_title("Cleaned")
-
-ax1.coords[1].set_ticks_visible(False)
-ax1.coords[1].set_ticklabel_visible(False)
+raster = read_files(raster_filename, spectral_windows="Si IV 1403")
+# Open the data and select one slice for the comparison.
+raster = raster["Si IV 1403"][10][4]
 
 ###############################################################################
-# For any cosmic ray removal, it is important to check the results. These
-# methods are not perfect and can sometimes mark real features as cosmic ray
-# hits, especially if the parameters are not tuned for the specific data.
+# Now we use ``remove_cosmic_rays`` .
+#
+# We suggest you experiment with the parameters to find the best set for your data.
+# We have found that the default parameters work well for most data, but in some cases, like this
+# one, you may want to be more aggressive in marking cosmic ray hits.
+
+raster_astroscrappy = raster.remove_cosmic_rays(method="astroscrappy")
+# We will use a larger kernel for rsliding, which makes it more aggressive in marking cosmic ray
+# hits. This will be a consequence of the spectral and spatial resolution of the data.
+raster_rsliding = raster.remove_cosmic_rays(method="rsliding", method_kwargs={"kernel": 5})
+
+###############################################################################
+# One reason to always be cautious when removing cosmic rays is that you can easily remove real features in the data if you are too aggressive.
+# For example, in this case, we have a strong Si IV 1403 line at around row 237, which is removed as a spike by both algorithms.
+# But we will look at a different row instead, which has a cleaner profile for Si IV 1403.
+
+si_iv_idx = 231
+
+fig, axes = plt.subplots(1, 3, figsize=(13, 4), constrained_layout=True)
+
+axes[0].imshow(raster.data, aspect="auto", vmin=0, vmax=500, origin="lower")
+axes[0].set_title("Original")
+axes[1].imshow(raster_astroscrappy.data, aspect="auto", vmin=0, vmax=500, origin="lower")
+axes[1].set_title("astroscrappy")
+axes[2].imshow(raster_rsliding.data, aspect="auto", vmin=0, vmax=500, origin="lower")
+axes[2].set_title("rsliding")
+
+for ax in axes:
+    ax.axhline(si_iv_idx, color="white", linestyle="--", linewidth=1)
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+###############################################################################
+# Finally, compare the line profile along the marked row.
+
+si_iv_wave = raster.axis_world_coords("wl")[0].to_value("angstrom")
+
+fig, ax = plt.subplots(1, 1, figsize=(7, 5), constrained_layout=True)
+ax.plot(si_iv_wave, raster.data[si_iv_idx, :], label="Original", linestyle="dotted", color="black")
+ax.plot(si_iv_wave, raster_astroscrappy.data[si_iv_idx, :], label="astroscrappy", linestyle="dashed")
+ax.plot(si_iv_wave, raster_rsliding.data[si_iv_idx, :], label="rsliding", linestyle="dashed")
+ax.set_xlabel("Wavelength (Å)")
+ax.set_xlim(1400, 1406)
+ax.legend()
+
+###############################################################################
+# In practice, the comparison is not about choosing one universally "better"
+# algorithm. You will need to run both, change the parameters, and visually
+# inspect the results to find the best solution for your data and science case.
 
 plt.show()
