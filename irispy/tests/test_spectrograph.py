83efd4c059cfcba7a8aa1bd55c7f0dc2f252e33d
Nabil Freij <nabil.freij@gmail.com>
2026-05-13 21:42:59 +0100

    Add error-case and real-data tests for spectral_dispersion and solid_angle

--- irispy/tests/test_spectrograph.py
+++ irispy/tests/test_spectrograph.py
@@ -1,11 +1,15 @@
 import copy
 
 import numpy as np
+import pytest
 
 import astropy.units as u
 from astropy.io import fits
+from astropy.wcs import WCS
 
 from irispy.io.spectrograph import read_spectrograph_lvl2
+from irispy.io.utils import read_files
+from irispy.spectrograph import SpectrogramCube
 from irispy.tests.helpers import make_test_spectrogram_cube
 from irispy.utils.constants import SLIT_WIDTH
 
@@ -104,3 +108,67 @@ def test_wavelength_axis():
     wavelengths = np.arange(10) * 0.02 * u.nm + 140 * u.nm
     cube = make_test_spectrogram_cube(np.ones((1, 1, 10)), wavelengths)
     assert cube.wavelength_axis == 2
+
+
+def test_spectral_dispersion_missing_wave_raises():
+    header = fits.Header()
+    header["NAXIS"] = 2
+    header["NAXIS1"] = 5
+    header["NAXIS2"] = 2
+    header["CTYPE1"] = "HPLT-TAN"
+    header["CTYPE2"] = "HPLN-TAN"
+    header["CDELT1"] = 0.1
+    header["CRVAL1"] = 0
+    header["CRPIX1"] = 1
+    header["CUNIT1"] = "arcsec"
+    header["CDELT2"] = 0.1
+    header["CRVAL2"] = 0
+    header["CRPIX2"] = 1
+    header["CUNIT2"] = "arcsec"
+    wcs = WCS(header)
+    cube = SpectrogramCube(np.ones((2, 5)), wcs=wcs, uncertainty=None, unit=u.DN, meta={}, mask=None)
+    with pytest.raises(ValueError, match="no WAVE ctype"):
+        _ = cube.spectral_dispersion
+
+
+def test_solid_angle_missing_hplt_raises():
+    header = fits.Header()
+    header["NAXIS"] = 3
+    header["NAXIS1"] = 5
+    header["NAXIS2"] = 2
+    header["NAXIS3"] = 1
+    header["CTYPE1"] = "WAVE"
+    header["CTYPE2"] = "TIME"
+    header["CTYPE3"] = "UTC"
+    header["CDELT1"] = 0.02
+    header["CRVAL1"] = 140.0
+    header["CRPIX1"] = 1
+    header["CUNIT1"] = "nm"
+    header["CDELT2"] = 1.0
+    header["CRVAL2"] = 0
+    header["CRPIX2"] = 1
+    header["CUNIT2"] = "s"
+    header["CDELT3"] = 1.0
+    header["CRVAL3"] = 0
+    header["CRPIX3"] = 1
+    header["CUNIT3"] = "s"
+    wcs = WCS(header)
+    cube = SpectrogramCube(np.ones((1, 2, 5)), wcs=wcs, uncertainty=None, unit=u.DN, meta={}, mask=None)
+    with pytest.raises(ValueError, match="no HPLT ctype"):
+        _ = cube.solid_angle
+
+
+def test_spectral_dispersion_real_data(sns_sg_file):
+    raster = read_files(sns_sg_file)
+    cube = raster["Si IV 1403"][0]
+    dispersion = cube.spectral_dispersion
+    assert dispersion.unit.is_equivalent(u.nm)
+    assert dispersion.value > 0
+
+
+def test_solid_angle_real_data(sns_sg_file):
+    raster = read_files(sns_sg_file)
+    cube = raster["Si IV 1403"][0]
+    angle = cube.solid_angle
+    assert angle.unit.is_equivalent(u.sr)
+    assert angle.value > 0
